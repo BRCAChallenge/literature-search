76ed2824965ac19cc69b0cf3eab327fafc2d4470
Audrey Musselman-Brown <almussel@ucsc.edu>
2017-04-25 16:34:27 +0000

    Update pdfbox version

--- docker/download.sh
+++ docker/download.sh
@@ -6,6 +6,6 @@ git clone -b brca --single-branch https://github.com/almussel/pubMunch.git $TMPD
 
 mkdir $TMPDIR/pubMunch/external
 mirror=$(python -c "from urllib2 import urlopen; import json; print json.load( urlopen('http://www.apache.org/dyn/closer.lua?path=$path&asjson=1'))['preferred']")
-wget -O $TMPDIR/pubMunch/external/pdfbox-app-2.0.4.jar ${mirror}pdfbox/2.0.4/pdfbox-app-2.0.4.jar
+wget -O $TMPDIR/pubMunch/external/pdfbox-app-2.0.5.jar ${mirror}pdfbox/2.0.5/pdfbox-app-2.0.5.jar
 wget -O $TMPDIR/pubMunch/external/docx2txt-1.4 https://sourceforge.net/projects/docx2txt/files/latest/download
 
