eaad2e2297e3ae7b3aaa8d6f9d895b143dcebc68
Audrey Musselman-Brown <almussel@ucsc.edu>
2017-04-28 20:03:19 +0000

    change pubmunch download to BRCA Challenge repository

--- docker/download.sh
+++ docker/download.sh
@@ -2,7 +2,7 @@
 
 TMPDIR=/tmp
 
-git clone -b brca --single-branch https://github.com/almussel/pubMunch.git $TMPDIR/pubMunch
+git clone https://github.com/BRCAChallenge/pubMunch-BRCA.git $TMPDIR/pubMunch
 
 mkdir $TMPDIR/pubMunch/external
 mirror=$(python -c "from urllib2 import urlopen; import json; print json.load( urlopen('http://www.apache.org/dyn/closer.lua?path=$path&asjson=1'))['preferred']")
