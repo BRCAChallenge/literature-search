8eaebe74147532c73c84bc90de0065c1fdb8af8a
Joseph Thomas <joathoma@ucsc.edu>
2018-07-13 01:45:26 +0000

    Update dependencies to docx2txt-1.4 and pdfbox 2.0.11

--- docker/download.sh
+++ docker/download.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-TMPDIR=/tmp
+TMPDIR=/home/joe/pmwork
+#PDFBOX="https://repo1.maven.org/maven2/org/apache/pdfbox/pdfbox-app/2.0.5/pdfbox-app-2.0.5.jar"
 
-git clone https://github.com/BRCAChallenge/pubMunch-BRCA.git $TMPDIR/pubMunch
+#git clone https://github.com/BRCAChallenge/pubMunch-BRCA.git $TMPDIR/pubMunch
+cp -R /home/joe/pubMunch-BRCA $TMPDIR/pubMunch
 
 mkdir $TMPDIR/pubMunch/external
 mirror=$(python -c "from urllib2 import urlopen; import json; print json.load( urlopen('http://www.apache.org/dyn/closer.lua?path=$path&asjson=1'))['preferred']")
-wget -O $TMPDIR/pubMunch/external/pdfbox-app-2.0.5.jar ${mirror}pdfbox/2.0.5/pdfbox-app-2.0.5.jar
-wget -O $TMPDIR/pubMunch/external/docx2txt-1.4 https://sourceforge.net/projects/docx2txt/files/latest/download
+wget -O $TMPDIR/pubMunch/external/pdfbox-app-2.0.11.jar ${mirror}pdfbox/2.0.11/pdfbox-app-2.0.11.jar
+wget -O $TMPDIR/pubMunch/external/docx2txt-1.4.tgz https://sourceforge.net/projects/docx2txt/files/latest/download
+tar -xzf $TMPDIR/pubMunch/external/docx2txt-1.4.tgz
 
--- lib/pubConf.py
+++ lib/pubConf.py
@@ -256,7 +256,7 @@ extToolDir = _sourceDir+"/external"
 # you can define variables and use them, see extToolDir
 CONVERTERS = {
     "doc":"catdoc $in > $out",
-    "docx":"%(extToolDir)s/docx2txt-1.2/docx2txt.pl < $in > $out",
+    "docx":"%(extToolDir)s/docx2txt-1.4/docx2txt.pl < $in > $out",
     "xls":"xls2csv $in > $out",
     "xlsx":"ssconvert $in $out",
     "ppt":"catppt $in > $out",
@@ -273,7 +273,7 @@ CONVERTERS = {
     "xml":"XMLTEXT",
     "nxml":"NXMLTEXT",
     "pdf":"pdftotext -q -nopgbrk -enc UTF-8 -eol unix $in $out",
-    "pdf2":"java -Xmx512m -jar %(extToolDir)s/pdfbox-app-2.0.5.jar ExtractText $in $out -encoding utf8"
+    "pdf2":"java -Xmx512m -jar %(extToolDir)s/pdfbox-app-2.0.11.jar ExtractText $in $out -encoding utf8"
 }
 
 # sometimes (e.g. if downloaded from the web) we don't have a file extension, but only 
