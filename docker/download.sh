c1b275f69affcbc8d2e264e23badc960f0d52e38
Joseph Thomas <joathoma@ucsc.edu>
2018-08-16 19:22:13 +0000

    fix tar command to untar docx2txt to correct path

--- docker/download.sh
+++ docker/download.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 TMPDIR=/tmp
-#TMPDIR=/home/joe/pmwork-merged
 #PDFBOX="https://repo1.maven.org/maven2/org/apache/pdfbox/pdfbox-app/2.0.5/pdfbox-app-2.0.5.jar"
 
 #git clone https://github.com/BRCAChallenge/pubMunch-BRCA.git $TMPDIR/pubMunch
@@ -12,5 +11,5 @@ mkdir $TMPDIR/pubMunch/external
 mirror=$(python -c "from urllib2 import urlopen; import json; print json.load( urlopen('http://www.apache.org/dyn/closer.lua?path=$path&asjson=1'))['preferred']")
 wget -O $TMPDIR/pubMunch/external/pdfbox-app-2.0.11.jar ${mirror}pdfbox/2.0.11/pdfbox-app-2.0.11.jar
 wget -O $TMPDIR/pubMunch/external/docx2txt-1.4.tgz https://sourceforge.net/projects/docx2txt/files/latest/download
-tar -xzf $TMPDIR/pubMunch/external/docx2txt-1.4.tgz
+tar -C $TMPDIR/pubMunch/external -xzf $TMPDIR/pubMunch/external/docx2txt-1.4.tgz
 
