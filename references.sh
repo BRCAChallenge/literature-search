d9280dc7f414dfe76e64a0484278fa6f3de2d7f5
Rob Currie <rcurrie@ucsc.edu>
2019-04-27 14:55:51 -0700

    Add back download of hg19

--- references.sh
+++ references.sh
@@ -6,4 +6,11 @@ wget -qO- http://hgwdev.soe.ucsc.edu/~max/pubs/tools/bigFiles.tgz \
   | tar xfzv - -C /references --strip-components 1
 cp -r /pubMunch/data/* /references/
 
+echo "Downloading hg19..."
+wget -N -P /references http://hgdownload.cse.ucsc.edu/goldenPath/hg19/bigZips/hg19.2bit
+wget -N -P /references http://hgdownload.cse.ucsc.edu/admin/exe/linux.x86_64/twoBitToFa
+chmod +x /references/twoBitToFa
+/references/twoBitToFa /references/hg19.2bit /references/variants/hg19.fa
+rm /references/hg19.2bit
+
 echo "Finished downloading references."
